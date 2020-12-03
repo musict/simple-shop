9e910cc0da5628726b00eac5f960616914eae601
musict <musicy@yandex.ru>
2020-12-03 10:37:15 +0300

    add email letter from cart. Edit cart js

--- js/cart.js
+++ js/cart.js
@@ -20,14 +20,19 @@ function showCart() {
 			var goods = data;
 			var out = '';
 			for (var id in cart){
-				out += `<button data-id="${id}" class="del-goods">x</button>`;
+				out += `<button data-id="${id}" class="del-goods"> x</button>`;
 				out += `<img src="images\\${goods[id].img}">`;
 				out += ` ${goods[id].name} `;
+				out += ` <button data-id="${id}" class="minus-goods"> -</button>`;
 				out	+= ` ${cart[id] }`;
+				out += ` <button data-id="${id}" class="plus-goods"> +</button>`; 
+				out += cart[id] * goods[id].cost;
 				out += '<br>';
 			}
 			$('.main-cart').html(out);
 			$('.del-goods').on('click', delGoods);
+			$('.plus-goods').on('click', plusGoods);
+			$('.minus-goods').on('click', minusGoods);
 		});
 	}
 
@@ -36,6 +41,25 @@ function showCart() {
 function delGoods() {
 	var id = $(this).attr('data-id');
 	delete cart[id];
+	saveCart();
+	showCart();
+}
+
+function plusGoods() {
+	var id = $(this).attr('data-id');
+	cart[id]++;
+	saveCart();
+	showCart();
+}
+
+function minusGoods() {
+	var id = $(this).attr('data-id');
+	if (cart[id] == 1) {
+		delete cart[id];
+	}
+	else {
+		cart[id]--;
+	}
 	saveCart();
 	showCart();
 }
@@ -51,6 +75,41 @@ function isEmpty(object) {
     return false;
 }
 
+function sendEmail() {
+	var name = $('#name').val();
+	var email = $('#email').val();
+	var phone = $('#phone').val();
+	if (name != '' && email != '' && phone != '') {
+		if (isEmpty(cart)) {
+			$.post(
+				"core/mail.php",
+				{
+					"name" : name,
+					"email" : email,
+					"phone" : phone,
+					"cart" : cart
+				},
+				function(data){
+					if (data == 1) {
+						alert('Заказа отправлен');
+					}
+					else {
+						alert('Ошибка, повторите заказ');
+					}
+				}
+			);
+		}
+		else{
+			alert('Корзина пуста')
+		}
+	}
+	else {
+		alert('Заполните поля');
+	}
+
+}
+
 $(document).ready(function (){
 	loadCart();
+	$('.send-email').on('click', sendEmail);
 });
